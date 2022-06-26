5a1886c656451c27b8b57d2af7cd992be62eb9f6
Keks <keks@htmlacademy.ru>
2022-06-26 19:01:05 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/main.js
@@ -0,0 +1,5 @@
+import {multipleObject} from './data.js';
+import './util.js';
+
+// eslint-disable-next-line no-unused-vars
+const generatedPhotos = multipleObject();
---
+++ 6/js/util.js
@@ -0,0 +1,20 @@
+// eslint-disable-next-line no-unused-vars
+function getRandomPositiveInteger (a, b) {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+}
+
+function maxLenStr(str, maxLen)
+{
+  return str.length <= maxLen;
+}
+
+maxLenStr('', 1);
+
+function getRandomArrayElement(ARR) {
+  return ARR[getRandomPositiveInteger(0, ARR.length-1)];
+}
+
+export {getRandomArrayElement, getRandomPositiveInteger, maxLenStr};
